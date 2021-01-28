326679071bc4201efd8590565a6b0c2e2db04b7a
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-28 11:21:01 -0500

    wip

--- controllers/users.js
+++ controllers/users.js
@@ -8,6 +8,7 @@ module.exports = {
 };
 
 async function signup(req, res) {
+  console.log('user')
   const user = new User(req.body);
   try {
     await user.save();
@@ -15,23 +16,23 @@ async function signup(req, res) {
     const token = createJWT(user);
     res.json({ token, user });
   } catch (err) {
+    console.log(err)
     // Probably a duplicate email
     res.status(400).json(err);
-  }
+}
 }
 
-console.log(400);
-
 async function login(req, res) {
+  
   try {
     const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(401).json({ err: "bad credentials "});
+    if (!user) return res.status(401).json({ err: "bad credentials" });
     user.comparePassword(req.body.password, (err, isMatch) => {
-      if (isMatch) {
-        const token = createJWT(user);
-        res.json({ token, user });
-      } else{
-        return res.status(401).json({ err: "bad credentials"});
+    if (isMatch) { 
+    const token = createJWT(user);
+    res.json({ token, user });
+      } else {
+        return res.status(401).json({ err: "bad credentials" });
       }
     });
   } catch (err) {
